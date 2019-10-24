095f6478ceb557a310dee89e10ad8f199a74a86e
Alexander Avramenko <alex.avr@gmail.com>
2019-10-24 17:02:04 +0700

    Add data from initial response

--- src/main/java/ru/quantum/dto/LoginResponse.java
+++ src/main/java/ru/quantum/dto/LoginResponse.java
@@ -4,7 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class LoginResponse {
-    String token;
-    List<String> cars = new ArrayList<>();
-    int level;
+    public String token;
+    public List<String> cars = new ArrayList<>();
+    public int level;
+    public List<RouteDTO> routes = new ArrayList<>();
+    public List<TrafficDTO> traffic = new ArrayList<>();
+    public List<PointDTO> points = new ArrayList<>();
 }
