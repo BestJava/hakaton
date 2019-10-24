59a4c4e47cf75ff9824f33d2b77e3a0d1d057da7
Vovan <sokolov_vovan@bk.ru>
2019-10-25 01:13:30 +0700

    обработчик событий

--- src/main/java/ru/quantum/domain/Car.java
+++ src/main/java/ru/quantum/domain/Car.java
@@ -1,4 +1,35 @@
 package ru.quantum.domain;
 
 public class Car {
+    private String name;
+    private double sum;
+    private int goPoint;
+
+    public Car(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getSum() {
+        return sum;
+    }
+
+    public void setSum(double sum) {
+        this.sum = sum;
+    }
+
+    public int getGoPoint() {
+        return goPoint;
+    }
+
+    public void setGoPoint(int goPoint) {
+        this.goPoint = goPoint;
+    }
 }
--- src/main/java/ru/quantum/domain/EdgeMap.java
+++ src/main/java/ru/quantum/domain/EdgeMap.java
@@ -10,6 +10,9 @@ public class EdgeMap extends AbstractEdgeMap {
     private RouteMap routeMap;
     private TrafficMap trafficMap;
 
+    public EdgeMap() {
+    }
+
     public EdgeMap(List<Route> routes) {
         routeMap = new RouteMap(routes);
     }
@@ -17,6 +20,10 @@ public class EdgeMap extends AbstractEdgeMap {
     public EdgeMap(List<Route> routes, List<Traffic> traffics) {
         this(routes);
         updateTraffic(traffics);
+    }
+
+    public void setRouteMap(List<Route> routes) {
+        this.routeMap = new RouteMap(routes);
     }
 
     public void updateTraffic(List<Traffic> traffics) {
@@ -32,7 +39,7 @@ public class EdgeMap extends AbstractEdgeMap {
         List<Double> routes = routeMap.routes(point);
         List<Double> traffics = trafficMap.routes(point);
 
-        for (int i = 0; i < routes.size(); i ++) {
+        for (int i = 0; i < routes.size(); i++) {
             routes.add(i, routes.get(i) * traffics.get(i));
         }
 
